da5708bbf869995fb8899c002bdb92df9917f8e2
AniruddhaChattopadhyay <studyaniruddha@gmail.com>
2019-11-10 02:34:55 +0530

    updated chrome driver and ERP code

--- ERP.py
+++ ERP.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
-import urllib.request
 import pyautogui
 import sys
 import time
 from PIL import ImageGrab
 import Captcha
+
 driver = webdriver.Chrome()
 driver.get("https://erp.iitkgp.ac.in/IIT_ERP3/showmenu.htm")
 driver.minimize_window()
@@ -23,7 +23,7 @@ time.sleep(1)
 question = driver.find_element_by_id("question")
 answer = driver.find_element_by_id("answer")
 q = question.text
-if(len(q)==0):
+if (len(q) == 0):
     print("Enter valid roll number")
     driver.close()
     sys.exit(1)
@@ -32,9 +32,10 @@ ans = input()
 answer.send_keys(ans)
 aca = driver.find_element_by_class_name("btn-primary").click()
 print("ATTENTION:")
-print("PLEASE DON'T MINIMIZE YOUR BROWSER AND KINDLY DON'T MOVE YOUR MOUSE/TOUCHPAD AND PLEASE DO NOT PRESS ANY KEY UNTIL THE BROWSER CLOSES")
+print(
+    "PLEASE DON'T MINIMIZE YOUR BROWSER AND KINDLY DON'T MOVE YOUR MOUSE/TOUCHPAD AND PLEASE DO NOT PRESS ANY KEY UNTIL THE BROWSER CLOSES")
 print("IN CASE OF ANY ERROR PLEASE KINDLY RUN THE ERP.exe FILE AGAIN")
-time.sleep(6)
+time.sleep(5)
 driver.maximize_window()
 try:
     driver.find_element_by_id("skiplink").click()
@@ -60,53 +61,58 @@ for el in fed:
                 time.sleep(.5)
                 break
         break
-#-------------------------------------------------------------Inside frame -----------------------------------------------------------
-frame=driver.switch_to.frame(driver.find_element_by_id('myframe'))
-fed4=driver.find_elements_by_tag_name("a")
+# -------------------------------------------------------------Inside frame -----------------------------------------------------------
+frame = driver.switch_to.frame(driver.find_element_by_id('myframe'))
+fed4 = driver.find_elements_by_tag_name("a")
 len_fed4 = len(fed4)
-for j in range(2):
-    for i in range(4):
+for j in range(4):
+    for i in range(6):
         for k in range(len_fed4):
-            fed4 = driver.find_elements_by_tag_name("a")
-            fed4[k].click()
-            time.sleep(.5)
-            fed5 = driver.find_elements_by_css_selector("input[type='radio'][name='check']")
             try:
-                    fed5[i].click()
-                    time.sleep(1)
-                    # -------------------------------------------------Captcha--------------------------------------------------------------------------
-                    img = driver.find_element_by_tag_name('img')
-                    actionChains = ActionChains(driver)
-                    actionChains.context_click(img).perform()
-                    time.sleep(.2)
-                    pyautogui.typewrite(['down'])
-                    time.sleep(.2)
-                    pyautogui.typewrite(['down'])
-                    time.sleep(.2)
-                    pyautogui.typewrite(['down'])
-                    time.sleep(.2)
-                    pyautogui.typewrite(['enter'])
+                fed4 = driver.find_elements_by_tag_name("a")
+                fed4[k].click()
+                time.sleep(.5)
+                fed5 = driver.find_elements_by_css_selector("input[type='radio'][name='check']")
+                # if (True):
+                fed5[i].click()
+                time.sleep(1)
+                # -------------------------------------------------Captcha--------------------------------------------------------------------------
+                img = driver.find_element_by_tag_name('img')
+                src = img.get_attribute('src')
+                #print(src)
+                driver.switch_to.default_content()
+                driver.execute_script("window.open('');")
+                time.sleep(3)
+                driver.switch_to.window(driver.window_handles[1])
+                driver.get(src)
+                time.sleep(3)
+                actionChains = ActionChains(driver)
+                actionChains.key_down(Keys.CONTROL).send_keys("c").key_up(Keys.CONTROL).perform()
+                driver.close()
+                time.sleep(3)
+                driver.switch_to.window(driver.window_handles[0])
+                driver.switch_to.frame(driver.find_element_by_id('myframe'))
+                img1 = ImageGrab.grabclipboard()
 
-                    time.sleep(.8)
-                    img1 = ImageGrab.grabclipboard()
+                img1.save('1.png', 'PNG')
+                txt = Captcha.break_captcha('1.png')
+                #print(txt)
+                fed8 = driver.find_element_by_name("passline")
+                fed8.send_keys(txt)
 
-                    img1.save('1.png', 'PNG')
-                    txt = Captcha.break_captcha('1.png')
-                    # print(txt)
-                    fed8 = driver.find_element_by_name("passline")
-                    fed8.send_keys(txt)
-
-                    fed6 = driver.find_elements_by_css_selector("input[type='radio'][value='5']")
-                    fed6 = fed6 + driver.find_elements_by_css_selector("input[type='radio'][value='53']")
-                    for el3 in fed6:
-                        el3.click()
-                    fed7 = driver.find_elements_by_tag_name("textarea")
-                    for el3 in fed7:
-                        el3.send_keys("No Comments")
-                    driver.find_element_by_id("sub").click()
-                    time.sleep(.5)
-                    pyautogui.typewrite(['enter'])
-                    time.sleep(.5)
+                fed6 = driver.find_elements_by_css_selector("input[type='radio'][value='5']")
+                fed6 = fed6 + driver.find_elements_by_css_selector("input[type='radio'][value='53']")
+                for el3 in fed6:
+                    el3.click()
+                fed7 = driver.find_elements_by_tag_name("textarea")
+                for el3 in fed7:
+                    el3.send_keys("No Comments")
+                driver.find_element_by_id("sub").click()
+                time.sleep(.5)
+                driver.switch_to.alert.accept()
+                time.sleep(.5)
+                driver.switch_to.frame(driver.find_element_by_id('myframe'))
+                time.sleep(.5)
             except:
                     pass
 
